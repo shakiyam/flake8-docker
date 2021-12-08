0d1487825c594ce51a7f48dea9d22b8d2e7051b7
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-12-08 16:48:39 +0900

    Support Podman

--- test_flake8.sh
+++ test_flake8.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -eu -o pipefail
 
-docker container run \
-  --name test_flake8$$ \
-  --rm \
-  -u "$(id -u):$(id -g)" \
-  -v "$PWD":/work:ro \
-  --entrypoint python3 \
-  shakiyam/flake8 ./test/test_flake8.py
+if [[ $(command -v podman) ]]; then
+  podman container run \
+    --name test_flake8$$ \
+    --rm \
+    --security-opt label=disable \
+    -v "$PWD":/work:ro \
+    --entrypoint python3 \
+    shakiyam/flake8 ./test/test_flake8.py "$@"
+else
+  docker container run \
+    --name test_flake8$$ \
+    --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$PWD":/work:ro \
+    --entrypoint python3 \
+    shakiyam/flake8 ./test/test_flake8.py
+fi
