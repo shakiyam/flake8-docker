8b79bd9ae02a9aaf903a1c620dbaa136e02be634
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2024-01-26 11:20:40 +0900

    Change command existence check method

--- test_flake8.sh
+++ test_flake8.sh
@@ -6,7 +6,7 @@ readonly SCRIPT_DIR
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR"/tools/colored_echo.sh
 
-if [[ $(command -v docker) ]]; then
+if command -v docker &>/dev/null; then
   docker container run \
     --entrypoint python3 \
     --name test_flake8$$ \
@@ -14,7 +14,7 @@ if [[ $(command -v docker) ]]; then
     -u "$(id -u):$(id -g)" \
     -v "$PWD":/work:ro \
     ghcr.io/shakiyam/flake8 ./test/test_flake8.py
-elif [[ $(command -v podman) ]]; then
+elif command -v podman &>/dev/null; then
   podman container run \
     --entrypoint python3 \
     --name test_flake8$$ \
